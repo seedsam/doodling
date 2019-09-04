e53340a2f26dffde7a2d9212191ece7c97251b74
DELL <DELL@DELL-PC>
2019-09-04 14:07:07 +0800

    multi thread

---
+++ demo/src/main/java/juc/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2011-2036 International Air Transport Association corp.
+ * All Rights Reserved.
+ */
+package juc;
+
+/**
+ * 
+ *
+ * @version Seurat v5.8
+ * @author Wei Yanqing, 2019-09-03
+ */
+public class Main {
+    
+
+}
