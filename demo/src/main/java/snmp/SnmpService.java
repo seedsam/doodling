97f7572038ad76d0388472808af7a9c0f65f1506
DELL <DELL@DELL-PC>
2019-09-04 18:06:51 +0800

    对外提供服务接口

---
+++ demo/src/main/java/snmp/SnmpService.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2011-2036 International Air Transport Association corp.
+ * All Rights Reserved.
+ */
+package snmp;
+
+/**
+ * 
+ *
+ * @version Seurat v5.8
+ * @author Wei Yanqing, 2019-09-04
+ */
+public interface SnmpService {
+
+}
---
+++ demo/src/main/java/snmp/impl/SnmpServiceImpl.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2011-2036 International Air Transport Association corp.
+ * All Rights Reserved.
+ */
+package snmp.impl;
+
+import snmp.SnmpService;
+
+/**
+ * 
+ *
+ * @version Seurat v5.8
+ * @author Wei Yanqing, 2019-09-04
+ */
+public class SnmpServiceImpl implements SnmpService{
+
+}
