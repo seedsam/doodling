ccf7c8dc1dc102ec2ef6d27650e34da784607a90
weiyq <weiyanqing@travelsky.com>
2019-11-04 20:01:17 +0800

    update

---
+++ demo/src/main/java/pattern/decorator/HouseBlend.java
@@ -0,0 +1,15 @@
+package pattern.decorator;
+
+public class HouseBlend extends Beverage {
+
+	public HouseBlend() {
+		description = "House Blend Coffe";
+	}
+
+	@Override
+	public double cost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
