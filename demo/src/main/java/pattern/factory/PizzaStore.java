8ad54400b0f1a8aed376fdfbc05b861805354cc9
weiyq <seedsam@163.com>
2019-11-08 21:28:43 +0800

    factory patter 03 取消GreekPizza 添加clamPizza 添加veggiePizza

--- demo/src/main/java/pattern/factory/PizzaStore.java
+++ demo/src/main/java/pattern/factory/PizzaStore.java
@@ -8,10 +8,14 @@ public class PizzaStore {
 
         if (type.equals("cheese")) {
             pizza = new CheesePizza();
-        } else if (type.equals("greek")) {
+        } /*else if (type.equals("greek")) {
             pizza = new GreekPizza();
-        } else if (type.equals("pepperoni")) {
+        }*/ else if (type.equals("pepperoni")) {
             pizza = new PepperoniPizza();
+        } else if (type.equals("clam")) {
+            pizza = new ClamPizza();
+        } else if (type.equals("Veggie")) {
+            pizza = new VeggiePizza();
         }
 
         pizza.prepare();
---
+++ demo/src/main/java/pattern/factory/VeggiePizza.java
@@ -0,0 +1,4 @@
+package pattern.factory;
+
+public class VeggiePizza extends Pizza {
+}
